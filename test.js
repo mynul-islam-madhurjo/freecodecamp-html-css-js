a27f7ca4ea8e0d01436f13069e8ef6ee3a7aea1e
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-05 00:50:21 +0600

    Es6 arrow fucntions

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 
 //Passing parameters
-(function myFunction(num1,num2) {
+/*(function myFunction(num1,num2) {
     console.log(num1,num2);
-})(1,2); // Passing arguments 1,2
+})(1,2); // Passing arguments 1,2*/
+
+// Arrow function
+ let myFunction2 = () => {
+     return 'working';
+ }
+ console.log(myFunction2());
 
