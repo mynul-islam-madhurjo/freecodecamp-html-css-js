5d75b0612575417990ee41b59f1b1ebdab4cac31
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-05 01:05:32 +0600

    Creating a function inside an object

--- test.js
+++ test.js
@@ -1,12 +1,30 @@
-
 //Passing parameters
+
 /*(function myFunction(num1,num2) {
     console.log(num1,num2);
-})(1,2); // Passing arguments 1,2*/
+})(1,2);*/ // Passing arguments 1,2
 
 // Arrow function
- let myFunction2 = () => {
-     return 'working';
- }
- console.log(myFunction2());
+/*let myFunction2 = () => {
+    return 'working';
+}*/
+ // console.log(myFunction2());
+
+//Creating a function inside an object
+let myFunction3 = () => {
+    return 'true';
+};
+
+const myObj = {
+    test: myFunction3()
+};
+
+console.log(myObj.test);
+
+
+
+
+
+
+
 
