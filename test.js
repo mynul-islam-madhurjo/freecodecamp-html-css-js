519f9acfc28966a9a94c4b05927bdba70b8d7947
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-05 00:25:44 +0600

    Passing parameters

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
-// Immediately Invoked function
-(function myFunction() {
-    console.log('test');
-})();
+//Passing parameters
+(function myFunction(num1,num2) {
+    console.log(num1,num2);
+})(1,2);
 
