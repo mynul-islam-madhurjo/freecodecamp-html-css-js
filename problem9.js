f027fc26b7dea1f7d8aa80c67241276a8ad7f43a
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-09 10:44:07 +0600

    problem 9 solve

---
+++ problem9.js
@@ -0,0 +1,15 @@
+/*
+It's the academic year's end, fateful moment of your school report.
+The averages must be calculated. All the students come to you and entreat you to calculate their average for them.
+Easy ! You just need to write a script. Return the average of the given array rounded down to its nearest integer.*/
+
+a = [1,2,3,4,5];
+function getAverage(marks){
+    //TODO : calculate the downward rounded average of the marks array
+    sum = 0;
+    for(let i= 0;i<marks.length;i++){
+        sum = sum+marks[i];
+    }
+    return Math.floor(sum/marks.length);
+}
+console.log(getAverage(a))
