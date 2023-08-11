f5d9bef330857dd4228386cacfe17903c8b91b96
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-12 00:15:59 +0600

    Date Objects

---
+++ Functions/date.js
@@ -0,0 +1,9 @@
+const now = new Date();
+
+console.log(now)
+console.log(now.valueOf()) //ms since jan 1 1970
+console.log(now.toString())
+console.log(now.toISOString())
+console.log(now.getDate())
+console.log(now.getFullYear())
+console.log(now.getMonth())
