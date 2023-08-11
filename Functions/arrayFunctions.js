a2d2d07fde27f38e1389c01e811ebb313c0e0a31
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-11 23:43:24 +0600

    Objects

--- Functions/arrayFunctions.js
+++ Functions/arrayFunctions.js
@@ -1,8 +1,22 @@
 array = [1,2,3]
 
-console.log('Map: = ' + array.map( x => x*2))
+// console.log('Map: = ' + array.map( x => x*2))
 
+// Objects
+const string1 = new String('Hello World');
+//Primitive (Data types like String, Number, Bigint, Boolean, Undefined, Symbol)
+//They are the simplest forms
+const string2 = 'Hello World';
 
+console.log(string1)
+console.log(string2)
+console.log(string1==string2)
+//returns false cause not the same type
+console.log(string1===string2)
+console.log('my'.toUpperCase()) // creates objects behind the scenes
+
+//Creating an instant of the object (Copy of the blueprint)
+const myDate = new Date();
 
 
 
