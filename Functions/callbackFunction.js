1146a7ceb330e2eb924527bcd51d6f46c2d28a30
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-11 21:09:21 +0600

    callback function

---
+++ Functions/callbackFunction.js
@@ -0,0 +1,18 @@
+function myCallBack(num){
+    return num * 2;
+}
+
+function mainFunction(randomNumber,call,callback){
+    let result = randomNumber;
+
+    if (call){
+        result = callback(result);
+    }
+
+    return result;
+}
+//Alternative
+// console.log(mainFunction(20,true,myCallBack))
+console.log(mainFunction(20,true, (num) =>{
+    return num*2;
+}))
