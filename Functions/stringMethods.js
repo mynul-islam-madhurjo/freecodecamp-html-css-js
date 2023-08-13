c121bf44f1fa153dd47ad551171b5a6d43508385
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-13 21:40:56 +0600

    string methods

---
+++ Functions/stringMethods.js
@@ -0,0 +1,9 @@
+// replaceAll, toUpperCase, substring, trim, match
+
+const myString = '     Madhurjo';
+
+//const newString = myString.replaceAll('M', 'm');
+console.log(myString.trim())
+console.log(myString.toUpperCase())
+console.log(myString.substring(1,-1))
+console.log(myString.match(/[a-z]+/))
