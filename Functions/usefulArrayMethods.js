000e4079f3fc1810691818c56ccadbe556516c1b
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-14 07:55:22 +0600

    array methods

---
+++ Functions/usefulArrayMethods.js
@@ -0,0 +1,19 @@
+// push() / pop () / shift() / unshift
+// slice()
+// splice
+// findIndex() / indexOf()
+// map ()
+// forEach()
+// includes()
+// filter()
+// reduce()
+
+const arr = [1,2,3]
+console.log(arr.pop()) // expected output 3
+console.log(arr.push(4))
+console.log(arr.unshift(0))
+console.log(arr.slice(0,2))
+console.log(arr.splice(3,1,3))
+console.log(arr.indexOf(0)) //findIndex() first item in array
+
+
